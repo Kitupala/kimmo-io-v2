cef49ee6bcc7c5731d0c718bf53cb9c3d40ff5bf
Kitupala <kimmo.tuupanen@gmail.com>
2023-08-19 16:46:42 +0300

    Fix styling

--- src/ui/Container.jsx
+++ src/ui/Container.jsx
@@ -2,7 +2,7 @@ import classNames from "classnames";
 
 function Container({ children, className }) {
   return (
-    <div className={classNames("mx-auto max-w-[100rem] px-8", className)}>
+    <div className={classNames("mx-auto max-w-[100rem] px-13", className)}>
       {children}
     </div>
   );
