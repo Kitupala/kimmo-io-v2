dbdabf46da22c1d7426962e0aea99b17acd99a65
Kitupala <kimmo.tuupanen@gmail.com>
2023-08-22 14:21:42 +0300

    Add dynamic content

--- src/features/projects/ProjectLayout.jsx
+++ src/features/projects/ProjectLayout.jsx
@@ -1,11 +1,19 @@
 import Sidenav from "../../ui/Sidenav";
 import Project from "./Project";
+import { useData } from "../../context/DataContext";
 
 function ProjectLayout() {
+  const { projects, projectTab, projectIsVisible } = useData();
+  const currentProject = projects
+    .filter((project) => project.id === projectTab)
+    .at();
+
   return (
     <div className="my-5 md:flex">
       <Sidenav />
-      <Project />
+      {projectIsVisible && (
+        <Project project={currentProject} key={currentProject.id} />
+      )}
     </div>
   );
 }
