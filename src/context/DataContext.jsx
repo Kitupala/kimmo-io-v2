cb9ce6e3aad0fb43bb0d73d86aa29f5cad1b4fc0
Kitupala <kimmo.tuupanen@gmail.com>
2023-08-22 14:13:44 +0300

    Add context for global state management

---
+++ src/context/DataContext.jsx
@@ -0,0 +1,53 @@
+import { createContext, useContext, useReducer } from "react";
+import { data } from "../data/data";
+
+const DataContext = createContext();
+
+const initialState = {
+  projects: [...data.projects],
+  studies: [...data.studies],
+  projectIsVisible: true,
+  projectTab: "0.1",
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "showProject":
+      return {
+        ...state,
+        projectIsVisible: state.projectTab === action.payload ? false : true,
+        projectTab: action.payload,
+      };
+
+    default:
+      throw new Error("Unknown error");
+  }
+}
+
+function DataProvider({ children }) {
+  const [{ projects, projectIsVisible, projectTab, studies }, dispatch] =
+    useReducer(reducer, initialState);
+
+  return (
+    <DataContext.Provider
+      value={{
+        projects,
+        projectIsVisible,
+        projectTab,
+        studies,
+        dispatch,
+      }}
+    >
+      {children}
+    </DataContext.Provider>
+  );
+}
+
+function useData() {
+  const context = useContext(DataContext);
+  if (context === undefined)
+    throw new Error("DataContext was used outside of DataProvider");
+  return context;
+}
+
+export { DataProvider, useData };
