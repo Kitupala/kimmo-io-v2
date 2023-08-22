277b9caa65daf5bcf08fb9548e67bf272504896e
Kitupala <kimmo.tuupanen@gmail.com>
2023-08-22 14:19:10 +0300

    Remove comment

--- src/features/projects/Projects.jsx
+++ src/features/projects/Projects.jsx
@@ -1,5 +1,4 @@
 import Container from "../../ui/Container";
-import { IoConstructOutline } from "react-icons/io5";
 import ProjectLayout from "./ProjectLayout";
 
 function Projects() {
@@ -7,17 +6,17 @@ function Projects() {
     <Container className="min-h-[calc(100vh-285px)] overflow-hidden">
       <div className="mt-16 sm:mt-24">
         <h2 className="text-gradient mt-5 translate-y-[-1rem] animate-fade-translate-in text-4xl tracking-tight opacity-0 [--animation-delay:200ms] sm:text-6xl md:mb-10">
-          Selected projects
+          Featured projects
         </h2>
       </div>
       <ProjectLayout />
 
       {/* Comment */}
-      <div className="mt-24">
+      {/* <div className="mt-24">
         <p className="mt-5 text-md text-sm italic text-grey sm:text-md">
           (Non-functional layout sample)
         </p>
-      </div>
+      </div> */}
     </Container>
   );
 }
