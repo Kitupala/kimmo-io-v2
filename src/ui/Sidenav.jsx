9e6c0def78a0b09d7ca9764ac28f2f527715c660
Kitupala <kimmo.tuupanen@gmail.com>
2023-08-24 13:15:24 +0300

    Fix error

--- src/ui/Sidenav.jsx
+++ src/ui/Sidenav.jsx
@@ -1,5 +1,5 @@
 import { useData } from "../context/DataContext";
-import SidenavButton from "./SidenavButton";
+// import SideNavButton from "./SideNavButton";
 
 function Sidenav() {
   const { projects } = useData();
@@ -8,13 +8,13 @@ function Sidenav() {
     <nav className="overflow-auto md:overflow-hidden">
       <ul className="mb-6 flex w-max text-md md:mb-0 md:w-[23rem] md:flex-col [&_button:hover]:border-l-slate-50/50 [&_button:hover]:text-grey md:[&_button:hover]:bg-transparent-white-primary [&_button]:duration-300 md:[&_button]:px-[2rem] [&_li]:px-[1.2rem]">
         {/* Mapping projects to render link buttons */}
-        {projects.map((project) => (
-          <SidenavButton
+        {/* {projects.map((project) => (
+          <SideNavButton
             id={project.id}
             title={project.title}
             key={project.id}
           />
-        ))}
+        ))} */}
       </ul>
     </nav>
   );
