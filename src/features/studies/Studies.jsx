132d8482a642d416aad94504777d9a4e31cf897a
Kitupala <kimmo.tuupanen@gmail.com>
2023-08-24 12:18:06 +0300

    Rename css class

--- src/features/studies/Studies.jsx
+++ src/features/studies/Studies.jsx
@@ -8,20 +8,20 @@ function Studies() {
 
   return (
     <Container className="min-h-[calc(100vh-285px)] overflow-hidden">
-      <div className="custom-bp:fixed mt-16 sm:mt-24">
+      <div className="custom-bp-md:fixed mt-16 sm:mt-24">
         <h2 className="text-gradient mb-6 translate-y-[-1rem] animate-fade-translate-in text-4xl tracking-tight opacity-0 [--animation-delay:200ms] sm:mb-8 sm:text-6xl md:mb-10">
           Coding courses
         </h2>
 
         <div className="relative animate-fade-in opacity-0 [--animation-delay:1500ms]">
-          <div className="custom-bp:block absolute -left-2 -top-60 hidden opacity-40">
+          <div className="custom-bp-md:block absolute -left-2 -top-60 hidden opacity-40">
             <Aphorism />
           </div>
         </div>
       </div>
 
-      <div className="custom-bp:justify-end custom-bp:mt-28 flex items-center">
-        <ol className="group/list custom-bp:max-w-2xl animate-fade-in opacity-0 [--animation-delay:800ms] lg:max-w-4xl">
+      <div className="custom-bp-md:justify-end custom-bp-md:mt-28 flex items-center">
+        <ol className="group/list custom-bp-md:max-w-2xl animate-fade-in opacity-0 [--animation-delay:800ms] lg:max-w-4xl">
           {studies.map((course) => (
             <Course course={course} key={course.id} />
           ))}
