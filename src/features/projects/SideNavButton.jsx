5591841eedf75bbd77dd31cdcc15420175f29aba
Kitupala <kimmo.tuupanen@gmail.com>
2023-08-25 10:59:41 +0300

    Add index as prop

--- src/features/projects/SideNavButton.jsx
+++ src/features/projects/SideNavButton.jsx
@@ -1,7 +1,7 @@
 import { useData } from "../../context/DataContext";
 import classNames from "classnames";
 
-function SideNavButton({ id, title }) {
+function SideNavButton({ id, title, index }) {
   const { dispatch, projectTab } = useData();
 
   function handleClick(e) {
@@ -9,23 +9,29 @@ function SideNavButton({ id, title }) {
     dispatch({ type: "showProject", payload: e.currentTarget.id });
   }
 
+  const animationStyle = {
+    animationDelay: `${index * 150}ms`,
+    animationFillMode: "both",
+  };
+
   return (
-    <div>
-      <li className="translate-y-[-1rem] animate-fade-translate-in opacity-0 [--animation-delay:200ms]">
-        <button
-          className={classNames(
-            "z-50 flex h-11 w-full items-center md:border-l-2",
-            projectTab === id
-              ? "pointer-events-none border-l-slate-50/50 text-[#858699]"
-              : "border-l-transparent-white-primary text-off-white",
-          )}
-          id={id}
-          onClick={handleClick}
-        >
-          <span>{title}</span>
-        </button>
-      </li>
-    </div>
+    <li
+      className="translate-y-[-1rem] animate-fade-translate-in opacity-0"
+      style={animationStyle}
+    >
+      <button
+        className={classNames(
+          "z-50 flex h-11 w-full items-center md:border-l-2",
+          projectTab === id
+            ? "pointer-events-none border-l-slate-50/50 text-[#858699]"
+            : "border-l-transparent-white-primary text-off-white",
+        )}
+        id={id}
+        onClick={handleClick}
+      >
+        <span>{title}</span>
+      </button>
+    </li>
   );
 }
 
