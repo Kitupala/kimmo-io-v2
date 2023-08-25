5883706cb14efd323157529e3e14314bb4f02a5e
Kitupala <kimmo.tuupanen@gmail.com>
2023-08-25 11:00:53 +0300

    Add index as prop

--- src/features/projects/SideNav.jsx
+++ src/features/projects/SideNav.jsx
@@ -8,10 +8,11 @@ function SideNav() {
     <nav className="overflow-auto md:overflow-hidden">
       <ul className="mb-6 flex w-max text-md md:mb-0 md:w-[23rem] md:flex-col [&_button:hover]:border-l-slate-50/50 [&_button:hover]:text-grey md:[&_button:hover]:bg-transparent-white-primary [&_button]:duration-300 md:[&_button]:px-[2rem] [&_li]:px-[1.2rem]">
         {/* Mapping projects to render link buttons */}
-        {projects.map((project) => (
+        {projects.map((project, i) => (
           <SideNavButton
             id={project.id}
             title={project.title}
+            index={i}
             key={project.id}
           />
         ))}
