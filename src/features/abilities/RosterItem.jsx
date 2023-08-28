4c54beb23ad9339454dcd2947f7be171901c33ef
Kitupala <kimmo.tuupanen@gmail.com>
2023-08-28 16:33:30 +0300

    Add new component

---
+++ src/features/abilities/RosterItem.jsx
@@ -0,0 +1,14 @@
+import { HiCheck } from "react-icons/hi";
+
+function RosterItem({ skill }) {
+  return (
+    <li className="group flex items-center gap-0.5 text-sm font-light leading-3 text-grey">
+      <span className="inline-block text-grey group-hover:text-primary-text">
+        <HiCheck />
+      </span>
+      <span className="group-hover:text-primary-text">{skill}</span>
+    </li>
+  );
+}
+
+export default RosterItem;
