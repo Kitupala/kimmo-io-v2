2c1bab152ffb1196dce2c2fbc87a8d0d398a9043
Kitupala <kimmo.tuupanen@gmail.com>
2023-08-28 16:34:27 +0300

    Add new content

--- src/features/abilities/Abilities.jsx
+++ src/features/abilities/Abilities.jsx
@@ -1,21 +1,38 @@
 import Container from "../../ui/Container";
-import { IoConstructOutline } from "react-icons/io5";
+import TechSkills from "./TechSkills";
+import RosterItem from "./RosterItem";
+
+import { useData } from "../../context/DataContext";
 
 function Abilities() {
-  return (
-    <Container className="min-h-[calc(100vh-285px)] overflow-hidden pt-16">
-      <div className="mt-6 flex flex-col text-center">
-        <h2 className="text-gradient my-6 text-6xl tracking-tight sm:text-7xl">
-          Sit tight.
-        </h2>
+  const { abilities } = useData();
+  const description = abilities.at(0).soft.at(0).description;
+  const softSkills = abilities.at(0).soft.at(1).roster;
 
-        <p className="fmx-5 flex items-center justify-center text-md text-primary-text sm:mx-20 sm:text-lg">
-          I&apos;m currently working on &lt;Abilities /&gt; page{" "}
-          <br className="block sm:hidden" /> and will launch ASAP...
-          <span className="animate-fade-in-out ml-2 inline-block flex hidden text-3xl md:block">
-            <IoConstructOutline />
-          </span>
-        </p>
+  return (
+    <Container className="min-h-[calc(100vh-285px)] overflow-hidden">
+      <div className="grid md:grid-cols-2">
+        {/* grid-col left*/}
+        <div className="row-span-full mt-16 sm:mt-24 md:col-start-1 md:col-end-2">
+          <h2 className="text-gradient mb-6 animate-fade-translate-in text-4xl tracking-tight opacity-0 [--animation-delay:200ms] sm:mb-8 sm:text-6xl md:mb-10">
+            Abilities and skills
+          </h2>
+
+          <p className="mb-8 animate-fade-translate-in pr-0 text-md text-primary-text opacity-0 [--animation-delay:300ms] md:mb-0  md:pr-8">
+            {description}
+          </p>
+        </div>
+
+        <div className="row-start-5 mt-8 flex animate-fade-translate-in opacity-0 [--animation-delay:500ms] md:col-end-2 md:row-start-1 md:mb-12 md:mr-12 md:mt-0 md:items-end">
+          <ul className="flex flex-wrap justify-center gap-3 md:justify-start">
+            {softSkills.map((skill) => (
+              <RosterItem skill={skill} key={skill} />
+            ))}
+          </ul>
+        </div>
+
+        {/* grid-col right*/}
+        <TechSkills />
       </div>
     </Container>
   );
