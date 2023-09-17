b10d3162bdf16de234432fd93b7dca1c5db7c597
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:47:25 +0300

    Fix styling

--- src/ui/HamburgerIcon.jsx
+++ src/ui/HamburgerIcon.jsx
@@ -1,6 +1,11 @@
-function HamburgerIcon() {
+function HamburgerIcon({ isDarkMode }) {
   return (
-    <svg width="18" height="11" viewBox="0 0 18 11" fill="currentColor">
+    <svg
+      width="18"
+      height="11"
+      viewBox="0 0 18 11"
+      className={isDarkMode ? "fill-highlight-text" : "fill-higlight-text"}
+    >
       <path d="M0 0H18V1H0V0Z"></path>
       <path d="M0 10H18V11H0V10Z"></path>
     </svg>
