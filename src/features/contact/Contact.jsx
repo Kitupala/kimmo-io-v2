5702fe75a7d9f6d75a7ad30283c3d91bdbf22248
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:37:44 +0300

    Add lightish theme

--- src/features/contact/Contact.jsx
+++ src/features/contact/Contact.jsx
@@ -12,7 +12,7 @@ function Contact() {
       <div className="m-auto mt-16 flex max-w-fit flex-col items-center sm:mt-24">
         {mailStatus?.type === "mailto" && (
           <>
-            <h2 className="text-gradient mb-6 animate-fade-translate-in self-start text-4xl tracking-tight opacity-0 [--animation-delay:200ms]  sm:text-6xl md:mb-0">
+            <h2 className="mb-6 animate-fade-translate-in self-start bg-gradient-to-br from-gradient-text from-25% to-gradient-text/[.45] bg-clip-text text-4xl tracking-tight text-transparent opacity-0 [--animation-delay:200ms] sm:text-6xl md:mb-0">
               Contact via email
             </h2>
             <ContactForm />
