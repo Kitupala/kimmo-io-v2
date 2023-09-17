94d5ac562cf72c0693ab91084e5739d55909ce5f
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:36:00 +0300

    Fix styling

--- src/features/abilities/RosterItem.jsx
+++ src/features/abilities/RosterItem.jsx
@@ -2,8 +2,8 @@ import { HiCheck } from "react-icons/hi";
 
 function RosterItem({ skill }) {
   return (
-    <li className="group flex cursor-default items-center gap-0.5 text-sm font-light leading-3 text-grey">
-      <span className="inline-block text-grey group-hover:text-primary-text">
+    <li className="text-muted-text group flex cursor-default items-center gap-0.5 text-sm font-light leading-3">
+      <span className="text-muted-text inline-block group-hover:text-primary-text">
         <HiCheck />
       </span>
       <span className="group-hover:text-primary-text">{skill}</span>
