8226a9d627820cdbe9dbfc1860382e135ab6519d
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:49:56 +0300

    Add lightish theme

--- src/ui/TechPill.jsx
+++ src/ui/TechPill.jsx
@@ -1,6 +1,18 @@
+import classNames from "classnames";
+import { useDarkMode } from "../context/DarkModeContext";
+
 function TechPill({ value }) {
+  const { isDarkMode } = useDarkMode();
+
   return (
-    <li className="mt-2 flex items-center rounded-full bg-page-gradient px-4 py-[6px] text-xs font-medium leading-5 text-grey">
+    <li
+      className={classNames(
+        "mt-2 flex items-center rounded-full px-4  py-[6px] text-xs font-medium leading-5  md:-mb-1",
+        isDarkMode
+          ? "bg-page-gradient text-indigo-500"
+          : "border-[0.5px] border-sky-300 bg-background/60 text-sky-600",
+      )}
+    >
       {value}
     </li>
   );
