369042f4096d6c7fd83d483d4754c94360681320
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:38:09 +0300

    Add lightish theme

--- src/features/contact/MailError.jsx
+++ src/features/contact/MailError.jsx
@@ -1,18 +1,35 @@
 import { BiError } from "react-icons/bi";
 import { useData } from "../../context/DataContext";
+import { useDarkMode } from "../../context/DarkModeContext";
+import classNames from "classnames";
 
 function MailError() {
   const { dispatch } = useData();
+  const { isDarkMode } = useDarkMode();
 
   return (
     <div className="flex min-h-[calc(100vh-365px)] animate-fade-in flex-col items-center justify-center opacity-0">
-      <BiError className="mb-6 text-7xl text-primary-text" />
+      <BiError
+        className={classNames(
+          "mb-6 text-7xl",
+          isDarkMode && "text-slate-500",
+          !isDarkMode && "text-sky-700",
+        )}
+      />
 
       <p className="mb-24 text-md text-primary-text">
         Something went wrong &mdash; <br className="sm:hidden" /> message was
         not delivered!
       </p>
-      <span className="max-w-max cursor-pointer rounded-md border border-transparent-white-secondary px-6 py-2.5 text-sm text-off-white transition-all duration-200 hover:border-indigo-900 hover:bg-page-gradient hover:text-indigo-300">
+      <span
+        className={classNames(
+          "max-w-max cursor-pointer rounded-md border border-transparent-md px-6 py-2.5 text-sm text-highlight-text transition-all duration-200",
+          isDarkMode &&
+            "hover:border-indigo-900 hover:bg-page-gradient hover:text-indigo-300",
+          !isDarkMode &&
+            "border-sky-300 hover:border-sky-400 hover:text-highlight-text",
+        )}
+      >
         <button
           onClick={() =>
             dispatch({ type: "setMailStatus", payload: { type: "mailto" } })
--- src/features/contact/MailSuccess.jsx
+++ src/features/contact/MailSuccess.jsx
@@ -1,20 +1,37 @@
 import { NavLink } from "react-router-dom";
 import { useData } from "../../context/DataContext";
 import ThumbsUp from "./ThumbsUp";
+import classNames from "classnames";
+import { useDarkMode } from "../../context/DarkModeContext";
 
 function MailSuccess() {
   const { dispatch } = useData();
+  const { isDarkMode } = useDarkMode();
 
   return (
     <div className="flex min-h-[calc(100vh-365px)] animate-fade-in flex-col items-center justify-center opacity-0">
-      <ThumbsUp className="mb-4 h-20 w-20 fill-grey sm:h-40 sm:w-40" />
+      <ThumbsUp
+        className={classNames(
+          "mb-4 h-20 w-20  sm:h-40 sm:w-40",
+          isDarkMode && "fill-slate-500",
+          !isDarkMode && "fill-sky-700",
+        )}
+      />
       <p className=" mb-4 text-center text-xl text-primary-text">
         Thank you for reaching out!
       </p>
       <p className="mb-24 text-center text-md text-primary-text">
         Your message just showed up in my inbox. Talk to you soon!
       </p>
-      <span className="max-w-max cursor-pointer rounded-md border border-transparent-white-secondary px-6 py-2.5 text-sm text-off-white transition-all duration-200 hover:border-indigo-900 hover:bg-page-gradient hover:text-indigo-300">
+      <span
+        className={classNames(
+          "max-w-max cursor-pointer rounded-md border border-transparent-md px-6 py-2.5 text-sm text-highlight-text transition-all duration-200",
+          isDarkMode &&
+            "hover:border-indigo-900 hover:bg-page-gradient hover:text-indigo-300",
+          !isDarkMode &&
+            "border-sky-300 hover:border-sky-400 hover:text-highlight-text",
+        )}
+      >
         <NavLink
           to="/"
           onClick={() =>
