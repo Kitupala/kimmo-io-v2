8cbea22b6b0844edee3b8a8ed65891e889b47c16
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:38:43 +0300

    Fix styling

--- src/features/projects/Projects.jsx
+++ src/features/projects/Projects.jsx
@@ -3,9 +3,9 @@ import ProjectLayout from "./ProjectLayout";
 
 function Projects() {
   return (
-    <Container className="min-h-[calc(100vh-285px)] overflow-hidden">
+    <Container className="min-h-[calc(100vh-280px)] overflow-hidden">
       <div className="mt-16 sm:mt-24">
-        <h2 className="text-gradient mt-5 translate-y-[-1rem] animate-fade-translate-in text-4xl tracking-tight opacity-0 [--animation-delay:200ms] sm:text-6xl md:mb-10">
+        <h2 className="mt-5 translate-y-[-1rem] animate-fade-translate-in bg-gradient-to-br from-gradient-text from-25% to-gradient-text/[.45] bg-clip-text text-4xl tracking-tight text-transparent opacity-0 [--animation-delay:200ms] sm:text-6xl md:mb-10">
           Featured projects
         </h2>
       </div>
