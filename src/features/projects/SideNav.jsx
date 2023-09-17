f398d0f2c5a64461da1c88768e255d7d1452ffbe
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:39:03 +0300

    Fix styling

--- src/features/projects/SideNav.jsx
+++ src/features/projects/SideNav.jsx
@@ -6,7 +6,8 @@ function SideNav() {
 
   return (
     <nav className="overflow-auto md:overflow-hidden">
-      <ul className="mb-6 flex w-max text-md md:mb-0 md:w-[23rem] md:flex-col [&_button:hover]:border-l-slate-50/50 [&_button:hover]:text-grey md:[&_button:hover]:bg-transparent-white-primary [&_button]:duration-300 md:[&_button]:px-[2rem] [&_li]:px-[1.2rem]">
+      <ul className="mb-6 flex w-max text-md md:mb-0 md:w-[23rem] md:flex-col md:[&_button]:px-[2rem] [&_li]:pl-[0.2rem] [&_li]:pr-[1.2rem]">
+        {/* <ul className="mb-6 flex w-max text-md md:mb-0 md:w-[23rem] md:flex-col [&_button:hover]:border-l-transparent-md [&_button:hover]:text-muted-text md:[&_button:hover]:bg-transparent-sm [&_button]:duration-300 md:[&_button]:px-[2rem] [&_li]:px-[1.2rem]"> */}
         {/* Mapping projects to render link buttons */}
         {projects.map((project, i) => (
           <SideNavButton
