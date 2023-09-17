95c185c36e3bcf84b0099b25edc3cb036cd179ba
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:36:38 +0300

    Fix styling

--- src/features/abilities/SkillPill.jsx
+++ src/features/abilities/SkillPill.jsx
@@ -1,6 +1,6 @@
 function SkillPill({ value }) {
   return (
-    <span className="ml-1 flex items-center rounded-md bg-page-gradient px-3 py-[6px] text-xs font-medium leading-5 text-primary-text group-hover:text-off-white">
+    <span className="ml-1 flex items-center rounded-md px-3  py-[6px] text-xs font-medium leading-5 text-primary-text group-hover:text-highlight-text">
       {value}
     </span>
   );
