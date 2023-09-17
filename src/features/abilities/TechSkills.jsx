e1db89a43f8f96110b643efe9067ecef2e7d653f
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:37:04 +0300

    Fix styling

--- src/features/abilities/TechSkills.jsx
+++ src/features/abilities/TechSkills.jsx
@@ -5,18 +5,18 @@ function TechSkills() {
   const { abilities } = useData();
 
   return (
-    <div className="animate-fade-translate-in rounded-md border border-transparent-white-primary bg-background/20 p-4 opacity-0 [--animation-delay:200ms] md:mt-24 md:animate-slide-in-right md:p-8">
+    <div className="animate-fade-translate-in rounded-md border border-transparent-sm bg-background/20 p-4 opacity-0 shadow-lg [--animation-delay:200ms] md:mb-6 md:mt-24 md:animate-slide-in-right md:p-8">
       <h3
         className="-mt-4 mb-6 text-xl text-primary-text sm:text-2xl 
   "
       >
-        <span className="text-xxs uppercase tracking-widest text-grey">
+        <span className="text-xxs uppercase tracking-widest text-muted-text">
           Hard
         </span>
         <br />
         Technical skills
       </h3>
-      <ul className="">
+      <ul>
         {abilities.at(0).tech.map((skill, i) => (
           <SkillBar
             title={skill.title}
