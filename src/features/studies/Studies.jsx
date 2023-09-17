cd8d34f750adf11b093e73d4864d400e24cd18c8
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:41:56 +0300

    Fix styling

--- src/features/studies/Studies.jsx
+++ src/features/studies/Studies.jsx
@@ -8,20 +8,20 @@ function Studies() {
 
   return (
     <Container className="min-h-[calc(100vh-285px)] overflow-hidden">
-      <div className="custom-bp-md:fixed mt-16 sm:mt-24">
-        <h2 className="text-gradient mb-6  animate-fade-translate-in text-4xl tracking-tight opacity-0 [--animation-delay:200ms] sm:mb-8 sm:text-6xl md:mb-10">
+      <div className="mt-16 sm:mt-24 custom-bp-md:fixed">
+        <h2 className="from-gradient-text to-gradient-text/[.45] mb-6 animate-fade-translate-in bg-gradient-to-br from-25% bg-clip-text text-4xl tracking-tight text-transparent opacity-0 [--animation-delay:200ms] sm:mb-8 sm:text-6xl md:mb-10">
           Coding courses
         </h2>
 
         <div className="relative animate-fade-in opacity-0 [--animation-delay:1500ms]">
-          <div className="custom-bp-md:block absolute -left-2 -top-60 hidden opacity-40">
+          <div className="absolute -left-2 -top-60 hidden opacity-40 custom-bp-md:block">
             <Aphorism />
           </div>
         </div>
       </div>
 
-      <div className="custom-bp-md:justify-end custom-bp-md:mt-28 flex items-center">
-        <ol className="group/list custom-bp-md:max-w-2xl animate-fade-in opacity-0 [--animation-delay:400ms] lg:max-w-4xl">
+      <div className="flex items-center custom-bp-md:mt-28 custom-bp-md:justify-end">
+        <ol className="group/list animate-fade-in opacity-0 [--animation-delay:400ms] custom-bp-md:max-w-2xl lg:max-w-4xl">
           {studies.map((course) => (
             <Course course={course} key={course.id} />
           ))}
