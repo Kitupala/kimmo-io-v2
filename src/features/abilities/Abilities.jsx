71f7ffda8b23d30e8d74b56f977e9dafeeb5a7ad
Kitupala <kimmo.tuupanen@gmail.com>
2023-09-17 21:35:34 +0300

    Fix styling

--- src/features/abilities/Abilities.jsx
+++ src/features/abilities/Abilities.jsx
@@ -14,21 +14,21 @@ function Abilities() {
       <div className="grid md:grid-cols-2">
         {/* grid-col left*/}
         <div className="row-span-full mt-16 sm:mt-24 md:col-start-1 md:col-end-2">
-          <h2 className="text-gradient mb-6 animate-fade-translate-in text-4xl tracking-tight opacity-0 [--animation-delay:200ms] sm:mb-8 sm:text-6xl md:mb-10">
+          <h2 className="mb-6 animate-fade-translate-in bg-gradient-to-br from-gradient-text from-25% to-gradient-text/[.45] bg-clip-text text-4xl tracking-tight text-transparent opacity-0 [--animation-delay:200ms] sm:mb-8 sm:text-6xl md:mb-10">
             Abilities and skills
           </h2>
 
-          <p className="mb-8 animate-fade-translate-in pr-0 text-md text-primary-text opacity-0 [--animation-delay:300ms] md:mb-0  md:pr-8">
+          <p className="mb-8 animate-fade-translate-in pr-0 text-md text-primary-text opacity-0 [--animation-delay:300ms] md:mb-8 md:pr-12">
             {description}
           </p>
-        </div>
 
-        <div className="row-start-5 mt-8 flex animate-fade-translate-in opacity-0 [--animation-delay:500ms] md:col-end-2 md:row-start-1 md:mb-12 md:mr-12 md:mt-0 md:items-end">
-          <ul className="flex flex-wrap justify-center gap-3 md:justify-start">
-            {softSkills.map((skill) => (
-              <RosterItem skill={skill} key={skill} />
-            ))}
-          </ul>
+          <div className="mt-8 flex hidden animate-fade-translate-in opacity-0 [--animation-delay:500ms] md:col-end-2 md:row-start-1  md:mr-12 md:mt-0 md:block">
+            <ul className="flex flex-wrap justify-center gap-3 md:justify-start">
+              {softSkills.map((skill) => (
+                <RosterItem skill={skill} key={skill} />
+              ))}
+            </ul>
+          </div>
         </div>
 
         {/* grid-col right*/}
